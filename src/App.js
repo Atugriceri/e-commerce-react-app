092211c3f79f26d97ca80faf6322e567da66a8a7
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-03-13 21:37:03 +0300

    font-family init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="bg-blue-500">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
