539d82b322785ceb96233558f7dfad018f02c086
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-03-13 21:27:54 +0300

    tailwind init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-blue-500">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
