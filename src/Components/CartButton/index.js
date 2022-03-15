0f500d2a9e697bd8535421fa009b0ffc36e6be33
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-03-15 13:25:20 +0300

    Cart Button Style Edits

--- src/Components/CartButton/index.js
+++ src/Components/CartButton/index.js
@@ -11,10 +11,10 @@ const CartButton = () => {
     <div className="justify-content-center mx-auto text-center">
       <div className={styles.cartMenu}>
         <Link to="/cart" className={styles.cartButton}>
-          <ShoppingCartIcon className="my-auto h-10 w-10 relative" aria-hidden="true"  />
+          <ShoppingCartIcon className={styles.cartIcon} aria-hidden="true"  />
           <div className="flex flex-col self-center"> 
               {items.length > 0 && (
-               <span className="bg-red-600 rounded-full w-4 h-4 text-xs text-center flex justify-center mx text-white absolute top-1.5 left-12">
+               <span className={styles.cartCount}>
                  {items.length}
                </span>
             )}
