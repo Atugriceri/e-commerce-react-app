1517bd5c3ecf2c2f7a40334adc1344b2e62fc367
Alper Tugriçeri <alpertugriceri@gmail.com>
2022-03-28 00:43:52 +0300

    styles edit

--- src/Pages/ProductDetail/index.js
+++ src/Pages/ProductDetail/index.js
@@ -16,49 +16,50 @@ const ProductDetail = () => {
   const findFavoriteItem = favoriteItems.find((item) => item.id === product.id);
 
   const { product_id } = useParams();
-  
+
   useEffect(() => {
     setProductID(product_id);
-  }, [])
+  }, []);
 
   return (
     <>
       {!loading && product?.id ? (
-        <div className="text-gray-700 body-font overflow-hidden">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="lg:w-3/5 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap max-w-7xl mx-auto my-4">
+            <div className="w-full sm:w-2/2 md:w-2/2 xl:w-5/5 p-4 flex flex-wrap">
               <img
                 alt="ecommerce"
-                className="lg:w-1/2 inset-0 max-h-max w-full py-4 my-auto px-4 object-scale-down rounded border border-gray-200"
+                className={styles.image}
                 src={product.image}
               />
-              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 className="text-sm title-font text-zinc-900/20 tracking-widest underline underline-offset-2 decoration-zinc-900 decoration-wavy mb-2 hover:text-blue-600">
+              <div className="lg:w-2/3 w-full lg:pl-10 lg:py-6 my-auto">
+                <h2 className={styles.brand}>
                   BRAND
                 </h2>
-                <h1 className="text-gray-900 text-2xl title-font font-bold tracking-tight mb-1">
+                <h1 className="text-gray-900 text-2xl font-bold tracking-tight mb-1">
                   {product.title}
                 </h1>
                 <div className={styles.rating} title={product?.rating?.rate}>
-            {[...Array(Math.round(product?.rating?.rate))].map((e, i) => (
-              <StarIcon
-                key={`star-${i}`}
-                className={styles.starIcon}
-                aria-hidden="true"
-              />
-            ))}
-            {[...Array(5 - Math.round(product?.rating?.rate))].map((e, i) => (
-              <StarIcon
-                key={`empty-star-${i}`}
-                className={styles.emptyStarIcon}
-                aria-hidden="true"
-              />
-            ))}
-            <p className="text-xs ml-1 font-light mt-0.5">
-              ({product?.rating?.count})
-            </p>
-          </div>
-                <p className="leading-relaxed border-b mb-4 border-zinc-900/10 border-offset-4 pb-6">
+                  {[...Array(Math.round(product?.rating?.rate))].map((e, i) => (
+                    <StarIcon
+                      key={`star-${i}`}
+                      className={styles.starIcon}
+                      aria-hidden="true"
+                    />
+                  ))}
+                  {[...Array(5 - Math.round(product?.rating?.rate))].map(
+                    (e, i) => (
+                      <StarIcon
+                        key={`empty-star-${i}`}
+                        className={styles.emptyStarIcon}
+                        aria-hidden="true"
+                      />
+                    )
+                  )}
+                  <p className="text-xs ml-1 font-light mt-0.5">
+                    ({product?.rating?.count})
+                  </p>
+                </div>
+                <p className={styles.productDetailText}>
                   {product.description}
                 </p>
                 <div className="flex">
@@ -78,7 +79,7 @@ const ProductDetail = () => {
                           className={styles.shoppingCartIcon}
                           aria-hidden="true"
                         />
-                        
+
                         <div className="flex flex-col self-center">
                           <span className={styles.buttonText}>
                             {findCartItem ? "Remove from cart" : "Add to Cart"}
@@ -87,30 +88,24 @@ const ProductDetail = () => {
                       </button>
                     </div>
                   </div>
-
-                  
-
                   <div className="block my-auto">
-                  <button
-                          className={
-                            !findFavoriteItem
-                              ? styles.favButton
-                              : styles.removeFavButton
-                          }
-                          onClick={() => {
-                            addToFavorite(product, findFavoriteItem)
-                          }}
-                        >
-                          <HeartIcon
-                            className={styles.heartIcon}
-                          />
-                        </button>
+                    <button
+                      className={
+                        !findFavoriteItem
+                          ? styles.favButton
+                          : styles.removeFavButton
+                      }
+                      onClick={() => {
+                        addToFavorite(product, findFavoriteItem);
+                      }}
+                    >
+                      <HeartIcon className={styles.heartIcon} />
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
       ) : (
         <Spinner />
       )}
@@ -118,4 +113,4 @@ const ProductDetail = () => {
   );
 };
 
-export default ProductDetail
+export default ProductDetail;
